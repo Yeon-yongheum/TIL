f3179fd489a354a52a4cd32a7067a76065ba43e6
Yeon-yongheum <yeon931@naver.com>
2019-07-23 17:56:52 +0900

    190723

---
+++ python/notes/my_package/math/calc.py
@@ -0,0 +1,22 @@
+def sum(a, b):
+    '''
+    더해요
+    '''
+    return a + b
+def sub(a, b):
+    '''
+    뺴요
+    '''
+    return a - b
+def mul(a, b):
+    '''
+    곱해요
+    '''
+    return a * b
+def div(a, b):
+    '''
+    나눠요
+    '''
+    if b == 0:
+        return False
+    return a / b
